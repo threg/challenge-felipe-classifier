a7c9c396a333595f9f54562e90fbf2581c2ce352
threg <reginattotalles@gmail.com>
2024-10-15 22:07:53 -0300

    entrega do primeiro projeto - classificador de heroi

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log("Oi")
+let nomeHeroi = "Talles"
+let xpHeroi = 5003
+let nivel = ""
+
+switch(true) {
+    case xpHeroi <= 1000:
+        nivel = "Ferro"
+        break
+    case xpHeroi <= 2000:
+        nivel = "Bronze"
+        break
+    case xpHeroi <= 5000:
+        nivel = "Prata"
+        break
+    case xpHeroi <= 7000:
+        nivel = "Ouro"
+        break
+    case xpHeroi <= 8000:
+        nivel = "Platina"
+        break
+    case xpHeroi <= 9000:
+        nivel = "Ascendente"
+        break
+    case xpHeroi <= 10000:
+        nivel = "Imortal"
+        break
+    case xpHeroi >= 10001:
+        nivel = "Radiante"
+        break
+}
+
+console.log("O Herói de nome " + nomeHeroi + " está no nivel de: " + nivel)
